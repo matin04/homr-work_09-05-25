fcd5bf7a2197f65f0a884a4ecfbfe7a76323ea35
matin04 <fathuddinzodamatin@gmail.com>
2025-05-09 09:38:54 +0300

    while 6

--- v.kh.py
+++ v.kh.py
@@ -1,14 +1,12 @@
-    # while 5 /////////////////////
+    # while 6 /////////////////////
 
 
 
 
 n=int(input())
 i=1
-while i<n:
+while i<=n:
+     print(i,end=' ')
      i*=2
-if i==n:
-    print('yes')
-else:
-     print('no')
+
    
