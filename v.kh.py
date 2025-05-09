f8d54332f4628a90716e7eba167b678f26b4dc76
matin04 <fathuddinzodamatin@gmail.com>
2025-05-09 10:17:28 +0300

    for 3

--- v.kh.py
+++ v.kh.py
@@ -1,12 +1,13 @@
-    # for 2 /////////////////////
+    # for 3 /////////////////////
 
 
 
 
 n=int(input())
-for i in range(1,n+1):
+for i in range(2,n+1):
     if n%i==0:
-        print(i,end=" ")
+        print(i)
+        break
 
 
 
